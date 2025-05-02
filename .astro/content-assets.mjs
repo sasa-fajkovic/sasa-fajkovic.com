341edeb28d9d4ee66e1f8686f2cb306205832b8e
Sasa Fajkovic <fajkovic.sasa@proton.me>
2025-05-02 21:04:47 +0200

    2025-05-01 AI autofixing dependency upgrade PR's (#3)

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,4 +1,5 @@
 
 import __ASTRO_IMAGE_IMPORT_N9CBK from "../../assets/images/posts/2025-01-06_old-website.png?astroContentImageFlag=&importer=src%2Fdata%2Fpost%2F2025-01-03-hello-world.md";
-export default new Map([["../../assets/images/posts/2025-01-06_old-website.png?astroContentImageFlag=&importer=src%2Fdata%2Fpost%2F2025-01-03-hello-world.md", __ASTRO_IMAGE_IMPORT_N9CBK]]);
+import __ASTRO_IMAGE_IMPORT_Z1lf6OC from "../../assets/images/posts/2025-05-01-mermaidchart.svg?astroContentImageFlag=&importer=src%2Fdata%2Fpost%2F2025-05-01-ai-powered-dependency-management.md";
+export default new Map([["../../assets/images/posts/2025-01-06_old-website.png?astroContentImageFlag=&importer=src%2Fdata%2Fpost%2F2025-01-03-hello-world.md", __ASTRO_IMAGE_IMPORT_N9CBK], ["../../assets/images/posts/2025-05-01-mermaidchart.svg?astroContentImageFlag=&importer=src%2Fdata%2Fpost%2F2025-05-01-ai-powered-dependency-management.md", __ASTRO_IMAGE_IMPORT_Z1lf6OC]]);
 		
